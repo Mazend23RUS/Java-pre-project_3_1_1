72a699ac9507997a59e36c226c4bcce15715fcc6
Алексей <bboy23@mail.ru>
2025-01-23 12:54:59 +0300

    Первый коммит

---
+++ src/main/java/com/semenov/Java_pre_project_3_1_1/JavaPreProject311Application.java
@@ -0,0 +1,13 @@
+package com.semenov.Java_pre_project_3_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPreProject311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPreProject311Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/semenov/Java_pre_project_3_1_1/JavaPreProject311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semenov.Java_pre_project_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPreProject311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
